d2455f206c3a8cd006d3062cf3f1838b25c706d6
Miledys Gomez <miledysgomez4@gmail.com>
2021-11-19 10:42:42 -0500

    Migracion base de datos

---
+++ database/migrations/2021_11_19_153414_create_usuario.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuario extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->String('Nombres');
+            $table->String('Apellidos');
+            $table->String('Edad');
+            $table->String('Tipo de sangre');
+            $table->String('Telefono/Celular');
+            $table->String('Numero cedula');
+            $table->String('¿Tiene hijos pequeños?');
+            $table->String('Tipo de serpiente');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario');
+    }
+}
