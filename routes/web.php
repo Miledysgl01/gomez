c6d13369653c95518b0d6115866e0e3a64808a4d
Miledys Gomez <miledysgomez4@gmail.com>
2021-11-19 10:14:50 -0500

    commit de ui

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+route:: get('/formulario', function() {
+    return view('formulario');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
